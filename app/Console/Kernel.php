36eda6d20b87115693ac45427806f5fc8385c5cd
odenigbo Godfrey <--global>
2021-11-18 02:55:06 +0100

    updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('todo:new')->everyMinute();
+        //$schedule->command('todo:new')->everyMinute();
+        $schedule->command('todo:compound_point')->monthly();
     }
 
     /**
